f376bbc80db5d0bb97202fba7b484bea68707fa9
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-04-10 15:08:10 -0700

    result table renders!!!

---
+++ hw19-employee-directory/src/components/Results.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "../styles/resultsList.css";
+
+// import Col from "./col";
+// import Row from "./row";
+// import Search from "./search";
+
+function Results({ persons }) {
+  console.log("from results", persons);
+  return (
+    <div className="container">
+      {" "}
+      <div className="results-table">
+        <table className="table table-striped table-dark">
+          <thead>
+            <tr>
+              <th scope="col">Image</th>
+              <th scope="col">Name</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Email</th>
+              <th scope="col">DOB</th>
+            </tr>
+          </thead>
+          <tbody>
+            {persons.map((person, index) => (
+              <tr key={index}>
+                <td>
+                  <img src={person.picture.medium} alt="person-photo" />{" "}
+                </td>
+                <td>{person.name.first}</td>
+                <td>{person.phone}</td>
+                <td>{person.email}</td>
+                <td>{person.dob.date.slice(0, 10)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {/* <Row>
+          <Col size="md-8"></Col>
+          <Col size="md-4"></Col>
+        </Row> */}
+    </div>
+  );
+}
+
+export default Results;
--- hw19-employee-directory/src/components/trackerComponent.jsx
+++ hw19-employee-directory/src/components/trackerComponent.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 import Header from "./header";
 import Search from "./search";
-import Results from "./resultsList";
+import Results from "./Results";
+import axios from "axios";
 
 class Tracker extends Component {
-  state = {};
+  state = { persons: [] };
+
+  componentDidMount() {
+    axios.get("https://randomuser.me/api/?results=10").then(({ data }) => {
+      const persons = data.results;
+      console.log("before set state", persons);
+      this.setState({ persons: persons });
+    });
+  }
+
   render() {
+    console.log("this.state.persons", this.state.persons);
     return (
       <div>
         <Header /> <Search />
-        <Results />
+        <Results persons={this.state.persons} />
       </div>
     );
   }
