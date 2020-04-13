012c87595fdd0aefa4c8fe643b47381700210e5c
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-04-13 16:12:26 -0700

    Updates

---
+++ precache-manifest.4eb8adebde7f09c0b6590ca4757889f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97b1b78685c514ffe5e1e92a549bc7f",
+    "url": "/HW19-employeeDirectory/index.html"
+  },
+  {
+    "revision": "dc5c4773806d38c1dfdc",
+    "url": "/HW19-employeeDirectory/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "51ed58792a5be3527c90",
+    "url": "/HW19-employeeDirectory/static/css/main.1e98954b.chunk.css"
+  },
+  {
+    "revision": "dc5c4773806d38c1dfdc",
+    "url": "/HW19-employeeDirectory/static/js/2.d7cf68ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW19-employeeDirectory/static/js/2.d7cf68ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ed58792a5be3527c90",
+    "url": "/HW19-employeeDirectory/static/js/main.5335a7bb.chunk.js"
+  },
+  {
+    "revision": "fbed70a99f235db82bd6",
+    "url": "/HW19-employeeDirectory/static/js/runtime-main.6fa29cb3.js"
+  },
+  {
+    "revision": "785afa839444fb949e4d692d913b42e9",
+    "url": "/HW19-employeeDirectory/static/media/ronny.785afa83.jpg"
+  }
+]);
